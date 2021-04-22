f1ce7159a9aec861e44fb60f858c07579ccc611e
Ritik-code <ritik.srivastava_cs18@gla.ac.in>
2021-04-22 14:37:34 +0530

    removed errors

--- server/index.js
+++ server/index.js
@@ -8,14 +8,15 @@ import userRouter from "./routes/user.js";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = "mongodb://root:";
+const CONNECTION_URL =
+  "mongodb+srv://AnonymousDatabase:Anonymous@123@cluster0.p2plb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
